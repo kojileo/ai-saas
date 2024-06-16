bb45307abf2afe6fe4f10272b1b2195d9a9bca75
kojileo <kojileo>
2024-06-16 16:58:05 +0900

    アプリ画面作成機能の実装

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -21,7 +21,7 @@ export const LandingNavbar = () => {
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
-        <Link href={"/dashboard"}>
+        <Link href={"/workspace"}>
           <Button variant="outline" className="rounded-full">
             Get Started
           </Button>
